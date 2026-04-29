06578e8285d7931dc7c635b6ad3bb1df5303e25d
Marcin <marcin.chudaszek@gmail.com>
2026-04-29 19:19:15 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/plantcare/sw.js', { scope: '/plantcare/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
